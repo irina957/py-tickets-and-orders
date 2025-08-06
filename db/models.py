c167ebdefc49b083d6d1e18e8e904137f381339b
iryna <irakirvas78@gmail.com>
2025-08-06 23:29:15 +0300

    Implement py-tickets-and-orders

--- db/models.py
+++ db/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class Genre(models.Model):
@@ -21,6 +25,9 @@ class Movie(models.Model):
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
+
+    class Meta:
+        indexes = [models.Index(fields=["title"])]
 
     def __str__(self) -> str:
         return self.title
@@ -50,3 +57,57 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey("MovieSession",
+                                      on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        form = self.movie_session.show_time.strftime("%Y-%m-%d %H:%M:%S")
+        return (f"{self.movie_session.movie}"
+                f" {form}"
+                f" (row: {self.row}, seat: {self.seat})")
+
+    def clean(self) -> None:
+        check_s = self.movie_session.cinema_hall.seats_in_row
+        check_r = self.movie_session.cinema_hall.rows
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError({"seat": f"seat number"
+                                           f" must be in available range"
+                                           f": (1, seats_in_row)"
+                                           f": (1,"
+                                           f" {check_s})"})
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError({"row": f"row number must be"
+                                          f" in available range"
+                                          f": (1, rows): (1,"
+                                          f" {check_r})"})
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [UniqueConstraint(fields=["row",
+                                                "seat", "movie_session"],
+                                        name="unique_seat_per_session")]
+
+
+class User(AbstractUser):
+    pass
---
+++ services/order.py
@@ -0,0 +1,39 @@
+from django.db import transaction
+from django.utils.dateparse import parse_datetime
+
+from db.models import Order, MovieSession, Ticket
+from django.contrib.auth import get_user_model
+
+
+def create_order(tickets: list, username: str, date: str = None) -> Order:
+    with transaction.atomic():
+        user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=user)
+        if date:
+            parsed_date = parse_datetime(date)
+            if parsed_date is None:
+                raise ValueError("Invalid date format. Use 'YYYY-MM-DD HH:MM'")
+            order.created_at = parsed_date
+            order.save(update_fields=["created_at"])
+
+        ticket_objects = []
+        for tic in tickets:
+            movie_session = MovieSession.objects.get(pk=tic["movie_session"])
+            ticket_objects.append(
+                Ticket(
+                    order=order,
+                    movie_session=movie_session,
+                    row=tic["row"],
+                    seat=tic["seat"]
+                )
+            )
+
+        Ticket.objects.bulk_create(ticket_objects)
+        return order
+
+
+def get_orders(username: str = None) -> Order:
+    result = Order.objects.all()
+    if username:
+        result = result.filter(user__username=username)
+    return result
